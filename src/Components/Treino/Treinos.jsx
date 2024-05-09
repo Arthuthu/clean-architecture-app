6686a4bfb84eaf5f319f9d63ea1a28bad1c92637
Arthuthu <arthurgeromello@hotmail.com>
2024-05-09 15:55:55 -0300

    Add treinos component

---
+++ src/Components/Treino/Treinos.jsx
@@ -0,0 +1,45 @@
+import GetAllTreinosService from '../../Services/TreinoService'
+
+export default function Treinos(){
+    const dataPromise = useLoaderData();
+    
+    function renderTreinosElements(treinos) {
+      return(
+        <>
+          <div className='users-title'>Seus treinos</div>
+          <div className='button-div'>
+            <Link className='create-user-button' to="../user/create">Adicionar</Link>
+          </div>
+          {treinos.map(user => (
+            <div key={user.id} className='users-list'>
+              <div className='users-list-row'>
+                <div className='users-list-label'>Nome</div>
+                <div className='user-name'>{user.username}</div>
+              </div>
+            </div>
+            ))}
+        </>
+      )
+    }
+  
+    return(
+      <>
+        <Suspense fallback={<Loading />}>
+          <Await resolve={dataPromise.treinos}>
+            { renderTreinosElements }
+          </Await>
+        </Suspense>
+      </>
+    )
+  }
+
+export async function loader({ request }){
+    const authenticationUrl = await Authenticate(request);
+
+    if(authenticationUrl != null)
+    {
+      return redirect(authenticationUrl);
+    }
+  
+    return defer({ treinos: GetAllTreinosService() });
+  }
--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,7 @@ import { action as createUserAction } from './Components/Users/CreateUser'
 import Login from './Components/Login/Login'
 import { action as loginAction, loader as loginLoader } from './Components/Login/Login'
 import Logout, { loader as logoutLoader } from './Components/Logout'
+import Treinos, { loader as treinosLoader } from './Components/Treino/Treinos'
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path='/' element={<Layout />}>
@@ -19,6 +20,7 @@ const router = createBrowserRouter(createRoutesFromElements(
     <Route path='user/:id' element={<UserDetail />} errorElement={<Error />} />
     <Route path='users' element={<Users />} loader={ usersLoader } errorElement={<Error />} />
     <Route path='user/create' element={<CreateUser />} action={ createUserAction } errorElement={<Error />} />
+    <Route path='treinos' element={<Treinos />} loader={ treinosLoader } errorElement={<Error />} />
     <Route path='login' element={<Login />} loader={ loginLoader } action={ loginAction } errorElement={<Error />} />
     <Route path='logout' element={<Logout />} loader={ logoutLoader } errorElement={<Error />} />
   </Route>
